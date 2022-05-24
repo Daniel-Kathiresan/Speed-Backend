7e854b7d0668483c7ba4b67b1f32f4f4adc2e70b
Daniel <daniel.kathiresan@gmail.com>
2022-05-24 17:53:56 +1200

    Adding fixes and pushing to heroku

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 var cors = require('cors');
 
 // routes
@@ -23,6 +25,17 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./speed-frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./speed-frontend/build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
